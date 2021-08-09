2a8ad38390840f68fffdd7c112ce82def1d3f1b4
Anna Kovalieva <annkov-0@student.ltu.se>
2021-08-09 18:28:06 +0200

    initial commit

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,5 +1,5 @@
 ﻿
-namespace inlamningsuppgift3_L0002B_annkov_0
+namespace inlamningsuppgift2_L0002B_annkov_0
 {
     partial class Form1
     {
@@ -29,13 +29,186 @@ namespace inlamningsuppgift3_L0002B_annkov_0
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.name_l = new System.Windows.Forms.Label();
+            this.lastName_l = new System.Windows.Forms.Label();
+            this.district_l = new System.Windows.Forms.Label();
+            this.sales_l = new System.Windows.Forms.Label();
+            this.name_tb = new System.Windows.Forms.TextBox();
+            this.lastName_tb = new System.Windows.Forms.TextBox();
+            this.district_tb = new System.Windows.Forms.TextBox();
+            this.sales_tb = new System.Windows.Forms.TextBox();
+            this.buttons_layout = new System.Windows.Forms.TableLayoutPanel();
+            this.next_button = new System.Windows.Forms.Button();
+            this.save_button = new System.Windows.Forms.Button();
+            this.info_l = new System.Windows.Forms.Label();
+            this.buttons_layout.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // name_l
+            // 
+            this.name_l.AutoSize = true;
+            this.name_l.Location = new System.Drawing.Point(12, 21);
+            this.name_l.Name = "name_l";
+            this.name_l.Size = new System.Drawing.Size(35, 13);
+            this.name_l.TabIndex = 0;
+            this.name_l.Text = "Name";
+            // 
+            // lastName_l
+            // 
+            this.lastName_l.AutoSize = true;
+            this.lastName_l.Location = new System.Drawing.Point(12, 70);
+            this.lastName_l.Name = "lastName_l";
+            this.lastName_l.Size = new System.Drawing.Size(55, 13);
+            this.lastName_l.TabIndex = 1;
+            this.lastName_l.Text = "Efternamn";
+            // 
+            // district_l
+            // 
+            this.district_l.AutoSize = true;
+            this.district_l.Location = new System.Drawing.Point(12, 121);
+            this.district_l.Name = "district_l";
+            this.district_l.Size = new System.Drawing.Size(39, 13);
+            this.district_l.TabIndex = 2;
+            this.district_l.Text = "Distrikt";
+            // 
+            // sales_l
+            // 
+            this.sales_l.AutoSize = true;
+            this.sales_l.Location = new System.Drawing.Point(11, 171);
+            this.sales_l.Name = "sales_l";
+            this.sales_l.Size = new System.Drawing.Size(31, 13);
+            this.sales_l.TabIndex = 3;
+            this.sales_l.Text = "Antal";
+            // 
+            // name_tb
+            // 
+            this.name_tb.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.name_tb.Location = new System.Drawing.Point(14, 37);
+            this.name_tb.Name = "name_tb";
+            this.name_tb.Size = new System.Drawing.Size(381, 20);
+            this.name_tb.TabIndex = 4;
+            // 
+            // lastName_tb
+            // 
+            this.lastName_tb.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lastName_tb.Location = new System.Drawing.Point(14, 87);
+            this.lastName_tb.Name = "lastName_tb";
+            this.lastName_tb.Size = new System.Drawing.Size(381, 20);
+            this.lastName_tb.TabIndex = 5;
+            // 
+            // district_tb
+            // 
+            this.district_tb.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.district_tb.Location = new System.Drawing.Point(13, 138);
+            this.district_tb.Name = "district_tb";
+            this.district_tb.Size = new System.Drawing.Size(382, 20);
+            this.district_tb.TabIndex = 6;
+            // 
+            // sales_tb
+            // 
+            this.sales_tb.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.sales_tb.Location = new System.Drawing.Point(15, 188);
+            this.sales_tb.Name = "sales_tb";
+            this.sales_tb.Size = new System.Drawing.Size(380, 20);
+            this.sales_tb.TabIndex = 7;
+            // 
+            // buttons_layout
+            // 
+            this.buttons_layout.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttons_layout.ColumnCount = 2;
+            this.buttons_layout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.buttons_layout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.buttons_layout.Controls.Add(this.next_button, 0, 0);
+            this.buttons_layout.Controls.Add(this.save_button, 1, 0);
+            this.buttons_layout.Location = new System.Drawing.Point(15, 229);
+            this.buttons_layout.Name = "buttons_layout";
+            this.buttons_layout.RowCount = 1;
+            this.buttons_layout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.buttons_layout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.buttons_layout.Size = new System.Drawing.Size(380, 64);
+            this.buttons_layout.TabIndex = 8;
+            // 
+            // next_button
+            // 
+            this.next_button.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.next_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.next_button.Font = new System.Drawing.Font("Roboto", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.next_button.ForeColor = System.Drawing.Color.AliceBlue;
+            this.next_button.Location = new System.Drawing.Point(3, 3);
+            this.next_button.Name = "next_button";
+            this.next_button.Size = new System.Drawing.Size(175, 58);
+            this.next_button.TabIndex = 0;
+            this.next_button.Text = "Next";
+            this.next_button.UseVisualStyleBackColor = false;
+            this.next_button.Click += new System.EventHandler(this.next_button_Click);
+            // 
+            // save_button
+            // 
+            this.save_button.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.save_button.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.save_button.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.save_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.save_button.Font = new System.Drawing.Font("Roboto", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.save_button.ForeColor = System.Drawing.Color.AliceBlue;
+            this.save_button.Location = new System.Drawing.Point(202, 3);
+            this.save_button.Name = "save_button";
+            this.save_button.Size = new System.Drawing.Size(175, 58);
+            this.save_button.TabIndex = 1;
+            this.save_button.Text = "Save to file";
+            this.save_button.UseVisualStyleBackColor = false;
+            this.save_button.Click += new System.EventHandler(this.save_button_Click);
+            // 
+            // info_l
+            // 
+            this.info_l.Location = new System.Drawing.Point(15, 353);
+            this.info_l.Name = "info_l";
+            this.info_l.Size = new System.Drawing.Size(376, 20);
+            this.info_l.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(408, 384);
+            this.Controls.Add(this.info_l);
+            this.Controls.Add(this.buttons_layout);
+            this.Controls.Add(this.sales_tb);
+            this.Controls.Add(this.district_tb);
+            this.Controls.Add(this.lastName_tb);
+            this.Controls.Add(this.name_tb);
+            this.Controls.Add(this.sales_l);
+            this.Controls.Add(this.district_l);
+            this.Controls.Add(this.lastName_l);
+            this.Controls.Add(this.name_l);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.buttons_layout.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label name_l;
+        private System.Windows.Forms.Label lastName_l;
+        private System.Windows.Forms.Label district_l;
+        private System.Windows.Forms.Label sales_l;
+        private System.Windows.Forms.TextBox name_tb;
+        private System.Windows.Forms.TextBox lastName_tb;
+        private System.Windows.Forms.TextBox district_tb;
+        private System.Windows.Forms.TextBox sales_tb;
+        private System.Windows.Forms.TableLayoutPanel buttons_layout;
+        private System.Windows.Forms.Button next_button;
+        private System.Windows.Forms.Button save_button;
+        private System.Windows.Forms.Label info_l;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,20 +1,203 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace inlamningsuppgift3_L0002B_annkov_0
+namespace inlamningsuppgift2_L0002B_annkov_0
 {
     public partial class Form1 : Form
     {
+        List<Seller> sellerHolder;
+        string formater = "{0,-30}\t{1,-15}\t{2,-15}\t{3,-5}\n";
+        const int salesLevels = 4;
         public Form1()
         {
             InitializeComponent();
+            sellerHolder = new List<Seller>();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void next_button_Click(object sender, EventArgs e)
+        {
+
+            if (InputValid())
+            {
+                Seller newSeller = SaveNewSellerTemp();
+                string message = newSeller.GetInfo() + " är tillagd!";
+                this.info_l.Text = message;
+                clearAllFields();
+            }
+            else
+            {
+                MessageBox.Show("Vänligen kontrollera input");
+            }
+        }
+
+        private Seller SaveNewSellerTemp()
+        {
+            Seller newSeller = new Seller()
+            {
+                Name = this.name_tb.Text,
+                Id = this.lastName_tb.Text,
+                District = this.district_tb.Text,
+                Sales = int.Parse(this.sales_tb.Text)
+            };
+            sellerHolder.Add(newSeller);
+            return newSeller;
+        }
+
+        private void clearAllFields()
+        {
+            this.name_tb.Text = String.Empty;
+            this.lastName_tb.Text = String.Empty;
+            this.district_tb.Text = String.Empty;
+            this.sales_tb.Text = String.Empty;
+        }
+
+        private void save_button_Click(object sender, EventArgs e)
+        {
+            if (hasInput())
+            {
+                if (InputValid())
+                {
+                    SaveNewSellerTemp();
+                }
+                else
+                {
+                    MessageBox.Show("Vänligen kontrollera input");
+                    return;
+                }
+            }
+            TestData();
+            sellerHolder.OrderBy(s => s.Sales);
+
+            StringBuilder builder = new StringBuilder();
+
+            builder.AppendFormat(formater, "Namn", "Personnr", "Distrikt", "Antal");
+            for (int levels = salesLevels; levels > 0; levels--)
+            {
+                builder.Append(GetAllSellersInALevelString(this.sellerHolder, levels));
+            }
+
+            MessageBox.Show(builder.ToString());
+
+        }
+
+        private string GetAllSellersInALevelString(List<Seller> sellersList, int level)
+        {
+            StringBuilder builder = new StringBuilder();
+            int minSales = GetMinMaxInLevel(level)[0];
+            int maxSales = GetMinMaxInLevel(level)[1];
+            int counter = 0;
+            foreach (Seller seller in sellersList)
+            {
+                if (seller.Sales >= minSales && seller.Sales <= maxSales)
+                {
+                    builder.AppendFormat(formater, seller.Name, seller.Id, seller.District, seller.Sales);
+                    counter++;
+                }
+            }
+
+            if (counter > 0)
+            {
+                builder.Append($"{counter} saljare har uppnat niva {level}\n\n");
+            }
+            return builder.ToString();
+        }
+
+        private int[] GetMinMaxInLevel(int level)
+        {
+            int min = 0;
+            int max = 0;
+
+            switch (level)
+            {
+                case 1:
+                    min = 0;
+                    max = 49;
+                    break;
+                case 2:
+                    min = 50;
+                    max = 99;
+                    break;
+                case 3:
+                    min = 100;
+                    max = 199;
+                    break;
+                case 4:
+                    min = 200;
+                    max = int.MaxValue;
+                    break;
+            }
+
+            int[] result = { min, max };
+            return result;
+        }
+
+        private bool InputValid()
+        {
+            int sales;
+            if (!int.TryParse(this.sales_tb.Text, out sales)) return false;
+            if (String.IsNullOrEmpty(this.lastName_tb.Text)) return false;
+            if (String.IsNullOrEmpty(this.district_tb.Text)) return false;
+            if (String.IsNullOrEmpty(this.name_tb.Text)) return false;
+            return true;
+        }
+
+        private bool hasInput()
+        {
+            if (!String.IsNullOrEmpty(this.sales_tb.Text)) return true;
+            if (!String.IsNullOrEmpty(this.lastName_tb.Text)) return true;
+            if (!String.IsNullOrEmpty(this.district_tb.Text)) return true;
+            if (!String.IsNullOrEmpty(this.name_tb.Text)) return true;
+            return false;
+        }
+
+        private void TestData()
+        {
+            Seller one = new Seller()
+            {
+
+                Name = "Marilyn Monroe",
+                Id = "92879376392",
+                District = "Hollywood",
+                Sales = 20
+            };
+
+            Seller two = new Seller()
+            {
+                Name = "Mahatma",
+                Id = "862693620",
+                District = "Bollywoon",
+                Sales = 80
+            };
+
+            Seller three = new Seller()
+            {
+                Name = "Darth Vader",
+                Id = "92879376392",
+                District = "Death Star",
+                Sales = 180
+            };
+
+            Seller four = new Seller()
+            {
+                Name = "Sherlock Holmes",
+                Id = "82036416",
+                District = "22 Baker St",
+                Sales = 380
+            };
+
+            sellerHolder.Add(three);
+            sellerHolder.Add(two);
+            sellerHolder.Add(four);
+            sellerHolder.Add(one);
+
         }
     }
 }
