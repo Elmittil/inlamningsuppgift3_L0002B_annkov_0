c1c0982157508905a0471e4bba0e64b14bb03561
Anna Kovalieva <annkov-0@student.ltu.se>
2021-08-10 10:51:34 +0200

    added person class

---
+++ Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inlamningsuppgift2_L0002B_annkov_0
+{
+    public class Seller
+    {
+        public string Name { get; set; }
+        public string Id { get; set; }
+        public string District { get; set; }
+        public int Sales { get; set; }
+
+        /// <summary>
+        /// return all fields as a string
+        /// </summary>
+        /// <returns>string containing Seller Name Id District and Sales amount</returns>
+        public string GetInfo()
+        {
+            string sellerInfo = $"{Name} {Id} {District} {Sales}";
+            return sellerInfo;
+        }
+    }
+}
